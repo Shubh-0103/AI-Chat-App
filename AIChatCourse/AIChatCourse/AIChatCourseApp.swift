5edde40d0122214331d71b307d76f6e8edadd3ba
SHUBH JAIN <shubhjain@SHUBHs-MacBook-Air.local>
2026-05-16 18:51:04 +0530

    Initial commit

---
+++ AIChatCourse/AIChatCourse/AIChatCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseApp.swift
+//  AIChatCourse
+//
+//  Created by SHUBH JAIN on 16/05/26.
+//
+
+import SwiftUI
+
+@main
+struct AIChatCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatCourse/AIChatCourseTests/AIChatCourseTests.swift
@@ -0,0 +1,18 @@
+//
+//  AIChatCourseTests.swift
+//  AIChatCourseTests
+//
+//  Created by SHUBH JAIN on 16/05/26.
+//
+
+import Testing
+
+struct AIChatCourseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
